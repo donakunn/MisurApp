92b0f96e4dc765298963819bfd67a4a36b8fea62
donakunn <kun@DESKTOP-5O8OFIM.localdomain>
2020-08-03 11:19:40 +0200

    merge codice Piera

---
+++ app/src/main/java/com/example/myapplication/DatabaseCaposcout.java
@@ -0,0 +1,97 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class DatabaseCaposcout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_caposcout);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        TableRow query;
+        TableRow.LayoutParams tableRowPar = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+
+        TextView nickname, data, strumento, valore;
+
+        ImageButton cancella;
+
+        int num_query = 3;
+
+        for(int i =0; i< num_query;i++){
+            query = new TableRow(DatabaseCaposcout.this);
+            query.setPadding(20,20,5,20);
+
+            nickname = new TextView(DatabaseCaposcout.this);
+            tableRowPar.weight = 1;
+            nickname.setLayoutParams(tableRowPar);
+            nickname.setGravity(Gravity.CENTER_VERTICAL);
+            nickname.setPadding(10,10,10,10);
+            nickname.setTextAppearance(R.style.textstyle);
+            nickname.setTypeface(null, Typeface.BOLD);
+            nickname.setText("nickname");
+
+            query.addView(nickname);
+
+            data = new TextView(DatabaseCaposcout.this);
+            tableRowPar.weight = 1;
+            data.setLayoutParams(tableRowPar);
+            data.setGravity(Gravity.CENTER_VERTICAL);
+            data.setPadding(10,10,10,10);
+            data.setTextAppearance(R.style.textstyle);
+            data.setTypeface(null, Typeface.BOLD);
+            data.setText("data");
+
+            query.addView(data);
+
+            strumento = new TextView(DatabaseCaposcout.this);
+            tableRowPar.weight = 1;
+            strumento.setLayoutParams(tableRowPar);
+            strumento.setGravity(Gravity.CENTER_VERTICAL);
+            strumento.setPadding(10,10,10,10);
+            strumento.setTextAppearance(R.style.textstyle);
+            strumento.setTypeface(null, Typeface.BOLD);
+            strumento.setText("strumento");
+
+            query.addView(strumento);
+
+            valore = new TextView(DatabaseCaposcout.this);
+            tableRowPar.weight = 1;
+            valore.setLayoutParams(tableRowPar);
+            valore.setGravity(Gravity.CENTER_VERTICAL);
+            valore.setTextAppearance(R.style.textstyle);
+            valore.setTypeface(null,Typeface.BOLD);
+            valore.setText("valore");
+
+            query.addView(valore);
+
+            cancella = new ImageButton(DatabaseCaposcout.this, null, R.style.buttondeletestyle);
+            cancella.setLayoutParams(tableRowPar);
+            cancella.setImageResource(R.drawable.ic_baseline_delete_24);
+
+            cancella.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    v.startAnimation(AnimationUtils.loadAnimation(DatabaseCaposcout.this, R.anim.button_click));
+
+                }
+            });
+
+            query.addView(cancella);
+            linearLayout.addView(query);
+
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import com.google.android.material.tabs.TabLayout;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,32 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id.tabs);//tabBar nel video
+        Button client = findViewById(R.id.Boyscout);
+        Button server = (Button)findViewById(R.id.Caposcout);
 
-        final ViewPager viewPager = findViewById(R.id.viewPager);
-        PageAdapter pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(pagerAdapter);
 
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+        client.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click));
+                Intent intent = new Intent(MainActivity.this,ListaStrumentiActivity.class);
+                startActivity(intent);
             }
         });
 
+        server.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.button_click));
+                Intent intent = new Intent(MainActivity.this,DatabaseCaposcout.class);
+                startActivity(intent);
+            }
+        });
 
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
@@ -53,51 +44,4 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 1);
     }//fine onCreate();
-
-    private class PageAdapter extends FragmentPagerAdapter {
-        private int numOfTabs;
-
-        public PageAdapter(FragmentManager fm, int numOfTabs) {
-            super(fm, numOfTabs);
-            this.numOfTabs = numOfTabs;
-        }
-
-
-        // Will be displayed as the tab's label
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch(position) {
-                case 0:
-                    return getResources().getString(R.string.Movimento);
-
-                case 1:
-                    return getResources().getString(R.string.Posizione);
-
-                case 2:
-                    return getResources().getString(R.string.Ambiente);
-
-                default:
-                    return null;
-            }
-        }
-        @Override
-        public Fragment getItem(int position) {
-
-            switch (position){
-                case 0:
-                    return new Fragment_listaMovimento();
-                case 1:
-                    return new Fragment_listaPosizione();
-                case 2:
-                    return new Fragment_listaAmbiente();
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return numOfTabs;
-        }
-    }
 }
